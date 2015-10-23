ed0b24bd5ae1d760a38270537961b2d130bed707
htchoi1995 <htchoi1995@gmail.com>
2015-10-23 14:36:03 +0200

    [1/2] SystemUI: Network speed indicator
    Change-Id: I88e7cdfe20342c695a33383db36bf80bb4ffe009

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3553,6 +3553,11 @@ public final class Settings {
          */
         public static final String ALLOW_LIGHTS = "allow_lights";
 
+	    /** Network speed indicator
+         * @hide
+         */
+        public static final String STATUS_BAR_TRAFFIC = "status_bar_traffic";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
