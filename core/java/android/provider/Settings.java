2a4ce7956ebb37ee5ba3742ad75a0a81b51ac566
rogersb11 <brettrogers11@gmail.com>
2015-10-30 15:32:33 +0100

    Custom toast text color 2/2

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5096,6 +5096,12 @@ public final class Settings {
         public static final String MEM_TEXT_COLOR = "mem_text_color";
 
         /**
+         * Color of toast message test
+         * @hide
+         */
+        public static final String TOAST_TEXT_COLOR = "toast_text_color";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
